6a1dbf4aa9a0bcd8edd1476b963087ea3f7bcd8d
Sojan <sojan@pepalo.com>
2022-08-03 16:51:32 +0200

    chore: Debug action cable

--- app/jobs/action_cable_broadcast_job.rb
+++ app/jobs/action_cable_broadcast_job.rb
@@ -2,8 +2,12 @@ class ActionCableBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(members, event_name, data)
+    logger.info "Processing ActionCableBroadcastJob"
+    logger.info "Available Channels: #{Redis.new.pubsub("channels", "*action_cable:*")}"
     members.each do |member|
-      ActionCable.server.broadcast(member, { event: event_name, data: data })
+      logger.info "Broadcasting to member: #{member}"
+      result = ActionCable.server.broadcast(member, { event: event_name, data: data })
+      logger.info "Broadcast result: #{result}"
     end
   end
 end
