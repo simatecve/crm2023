5dd58102e01aafacc5036b19653c01f579d17417
Pranav Raj S <pranav@chatwoot.com>
2022-12-14 18:53:47 -0800

    Revert "fix: Autoloading during initialization deprecation warning (#5628)"
    This reverts commit 86ca7f4a8d5e2b7d45033e85d5e100870ed0bfa0.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,6 +12,4 @@ Sidekiq.configure_server do |config|
 end
 
 # https://github.com/ondrejbartas/sidekiq-cron
-Rails.application.reloader.to_prepare do
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
-end
+Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
