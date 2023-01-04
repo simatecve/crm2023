25631c083843ee249bca232c458232556887d146
Pranav Raj S <pranav@chatwoot.com>
2023-01-03 23:59:05 -0800

    Added channel creation flow

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Email.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Email.vue
@@ -5,6 +5,7 @@
   >
     <div class="row channels">
       <div
+        v-if="globalConfig.azureAppId"
         class="small-6 medium-4 large-3 columns channel"
         @click="onMicrosoftClick"
       >
@@ -29,6 +30,9 @@
 <script>
 import * as msal from '@azure/msal-browser';
 import ForwardToOption from './Email/ForwardToOption.vue';
+import { mapGetters } from 'vuex';
+import router from '../../../..';
+
 export default {
   components: {
     ForwardToOption,
@@ -36,13 +40,20 @@ export default {
   data() {
     return { forwardTo: false };
   },
+  computed: {
+    ...mapGetters({ globalConfig: 'globalConfig/get' }),
+  },
   methods: {
     async onMicrosoftClick() {
       try {
         const msalConfig = {
+<<<<<<< HEAD
           auth: {
             clientId: '67fa631b-e958-4ccb-95f0-ecede13d53ce',
           },
+=======
+          auth: { clientId: this.globalConfig.azureAppId },
+>>>>>>> df7e28fb3 (Added channel creation flow)
         };
 
         const msalInstance = new msal.PublicClientApplication(msalConfig);
@@ -52,10 +63,35 @@ export default {
             'https://outlook.office.com/IMAP.AccessAsUser.All',
             'https://outlook.office.com/SMTP.Send',
           ],
-          redirectUri: 'http://localhost:3000',
+          redirectUri: window.location.origin,
         });
 
+<<<<<<< HEAD
         console.log(loginResponse);
+=======
+        const { account: { username, name } = {} } = loginResponse;
+
+        const emailChannel = await this.$store.dispatch(
+          'inboxes/createChannel',
+          {
+            name,
+            channel: {
+              type: 'email',
+              email: username,
+              provider: 'microsoft',
+              provider_config: loginResponse,
+            },
+          }
+        );
+
+        router.replace({
+          name: 'settings_inboxes_add_agents',
+          params: {
+            page: 'new',
+            inbox_id: emailChannel.id,
+          },
+        });
+>>>>>>> df7e28fb3 (Added channel creation flow)
       } catch (err) {
         // handle error
       }
